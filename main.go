9e10163a97d34e66b4a904bcb3352d0bcd0b6904
zachklingbeil <zach@zachklingbeil.com>
2025-10-17 16:48:40 -0700

    added logic for minifying pathless and gzip compression. pathless.html is ~8.1kb, minified its ~6.2kb, gzip makes it a ~2.1kb payload.

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
 	_ "embed"
 	"html/template"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -34,7 +37,31 @@ func init() {
 	}); err != nil {
 		panic("template execute error: " + err.Error())
 	}
-	one = []byte(b.String())
+
+	minified := minify(b.String())
+	var buf bytes.Buffer
+	gz := gzip.NewWriter(&buf)
+	if _, err := gz.Write([]byte(minified)); err != nil {
+		panic("gzip write error: " + err.Error())
+	}
+	if err := gz.Close(); err != nil {
+		panic("gzip close error: " + err.Error())
+	}
+	one = buf.Bytes()
+}
+
+func minify(html string) string {
+	// Remove HTML comments
+	html = regexp.MustCompile(`<!--[\s\S]*?-->`).ReplaceAllString(html, "")
+	// Remove whitespace between tags
+	html = regexp.MustCompile(`>\s+<`).ReplaceAllString(html, "><")
+	// Collapse multiple spaces/tabs/newlines to single space
+	html = regexp.MustCompile(`\s+`).ReplaceAllString(html, " ")
+	// Remove spaces around specific characters
+	html = strings.ReplaceAll(html, " >", ">")
+	html = strings.ReplaceAll(html, "< ", "<")
+	// Trim leading/trailing whitespace
+	return strings.TrimSpace(html)
 }
 
 func Pathless(w http.ResponseWriter, r *http.Request) {
@@ -43,6 +70,7 @@ func Pathless(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Encoding", "gzip")
 	w.Write(one)
 }
 
