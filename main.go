cab1d88b6cfdfb91b6f664516df47eedb1681c7f
zachklingbeil <zach@zachklingbeil.com>
2025-11-05 09:07:17 -0800

    gzip moved from init to minify.

--- main.go
+++ main.go
@@ -17,42 +17,48 @@ var one []byte
 
 func init() {
 	title := os.Getenv("TITLE")
+	apiURL := os.Getenv("API_URL")
+
 	if title == "" {
 		title = "hello_universe"
 	}
-	apiURL := os.Getenv("API_URL")
 	if apiURL == "" {
 		apiURL = "http://localhost:1002"
 	} else if !strings.HasPrefix(apiURL, "http://") && !strings.HasPrefix(apiURL, "https://") {
 		apiURL = "https://" + apiURL
 	}
 
-	tmpl, err := template.New("page").Parse(zero)
+	pathless, err := template.New("pathless").Parse(zero)
 	if err != nil {
 		panic("template parse error: " + err.Error())
 	}
 
 	var b strings.Builder
-	if err := tmpl.Execute(&b, map[string]string{
+	if err := pathless.Execute(&b, map[string]string{
 		"Title":  title,
 		"APIURL": apiURL,
 	}); err != nil {
 		panic("template execute error: " + err.Error())
 	}
-
-	minified := minify(b.String())
-	var buf bytes.Buffer
-	gz := gzip.NewWriter(&buf)
-	if _, err := gz.Write([]byte(minified)); err != nil {
-		panic("gzip write error: " + err.Error())
-	}
-	if err := gz.Close(); err != nil {
-		panic("gzip close error: " + err.Error())
-	}
-	one = buf.Bytes()
+	one = minify(b.String())
 }
 
-func minify(html string) string {
+func Pathless(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" || r.URL.RawQuery != "" {
+		http.Redirect(w, r, "/", http.StatusMovedPermanently)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Encoding", "gzip")
+	w.Write(one)
+}
+
+func main() {
+	http.HandleFunc("/", Pathless)
+	http.ListenAndServe(":1001", nil)
+}
+
+func minify(html string) []byte {
 	// Minify CSS in <style> tags
 	html = regexp.MustCompile(`<style>([\s\S]*?)</style>`).ReplaceAllStringFunc(html, func(s string) string {
 		s = regexp.MustCompile(`/\*[\s\S]*?\*/`).ReplaceAllString(s, "")    // Remove CSS comments
@@ -75,32 +81,24 @@ func minify(html string) string {
 
 	// Remove whitespace between HTML tags
 	html = regexp.MustCompile(`>\s+<`).ReplaceAllString(html, "><")
-
 	// Collapse multiple spaces/newlines to single space
 	html = regexp.MustCompile(`\s+`).ReplaceAllString(html, " ")
-
 	// Remove spaces around tag brackets
 	html = strings.ReplaceAll(html, " >", ">")
 	html = strings.ReplaceAll(html, "< ", "<")
-
 	// Remove optional quotes around simple attribute values
 	html = regexp.MustCompile(`=["']([a-zA-Z0-9\-_]+)["']`).ReplaceAllString(html, "=$1")
-
 	// Trim leading/trailing whitespace
-	return strings.TrimSpace(html)
-}
+	html = strings.TrimSpace(html)
 
-func Pathless(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" || r.URL.RawQuery != "" {
-		http.Redirect(w, r, "/", http.StatusMovedPermanently)
-		return
+	// Gzip compress the minified HTML
+	var buf bytes.Buffer
+	gz := gzip.NewWriter(&buf)
+	if _, err := gz.Write([]byte(html)); err != nil {
+		panic("gzip write error: " + err.Error())
 	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.Header().Set("Content-Encoding", "gzip")
-	w.Write(one)
-}
-
-func main() {
-	http.HandleFunc("/", Pathless)
-	http.ListenAndServe(":1001", nil)
+	if err := gz.Close(); err != nil {
+		panic("gzip close error: " + err.Error())
+	}
+	return buf.Bytes()
 }
